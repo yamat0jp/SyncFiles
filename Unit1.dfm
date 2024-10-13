785ac0c407524566c9a357bc1abe52785963bc24
yamat0jp <yamat0jp@yahoo.co.jp>
2024-10-14 04:54:53 +0900

    first commit

---
+++ Unit1.dfm
@@ -0,0 +1,117 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  BorderStyle = bsSingle
+  Caption = 'SyncFiles'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Menu = MainMenu1
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  TextHeight = 15
+  object ListBox1: TListBox
+    Left = 0
+    Top = 0
+    Width = 624
+    Height = 121
+    Align = alTop
+    ItemHeight = 15
+    TabOrder = 0
+    OnKeyDown = ListBox1KeyDown
+    ExplicitLeft = 1
+    ExplicitTop = -5
+  end
+  object Panel1: TPanel
+    Left = 0
+    Top = 121
+    Width = 624
+    Height = 320
+    Align = alClient
+    Caption = 'Panel1'
+    TabOrder = 1
+    object ListBox2: TListBox
+      Left = 1
+      Top = 42
+      Width = 500
+      Height = 277
+      Align = alLeft
+      ItemHeight = 15
+      TabOrder = 0
+    end
+    object ListBox3: TListBox
+      Left = 501
+      Top = 42
+      Width = 122
+      Height = 277
+      Align = alClient
+      ItemHeight = 15
+      TabOrder = 1
+    end
+    object Panel2: TPanel
+      Left = 1
+      Top = 1
+      Width = 622
+      Height = 41
+      Align = alTop
+      TabOrder = 2
+      object Label1: TLabel
+        Left = 24
+        Top = 20
+        Width = 50
+        Height = 15
+        Caption = #12513#12483#12475#12540#12472
+      end
+    end
+  end
+  object FileOpenDialog1: TFileOpenDialog
+    FavoriteLinks = <>
+    FileName = 'C:\Users\yamat\Documents\GitHub\SyncFiles'
+    FileTypes = <
+      item
+        DisplayName = #23455#34892#12501#12449#12452#12523
+        FileMask = '*.dpr;*.dproj'
+      end
+      item
+        DisplayName = #12377#12409#12390#12398#12501#12449#12452#12523
+        FileMask = '*.*'
+      end>
+    Options = []
+    Title = #23550#35937#12450#12503#12522#12398#36984#25246
+    Left = 200
+    Top = 192
+  end
+  object MainMenu1: TMainMenu
+    Left = 464
+    Top = 168
+    object N1: TMenuItem
+      Caption = #30331#37682
+      OnClick = N1Click
+    end
+    object N4: TMenuItem
+      Caption = #20877#26908#32034
+      OnClick = N4Click
+    end
+    object N5: TMenuItem
+      Caption = #23455#34892
+      OnClick = N5Click
+    end
+    object N6: TMenuItem
+      Caption = #34920#31034
+      OnClick = N6Click
+    end
+    object N3: TMenuItem
+      Caption = #36984#25246#23455#34892
+      OnClick = N3Click
+    end
+    object N2: TMenuItem
+      Caption = #32066#20102
+      OnClick = N2Click
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,245 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, JvComponentBase, JvDragDrop,
+  Vcl.StdCtrls, Vcl.Menus, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    ListBox1: TListBox;
+    ListBox2: TListBox;
+    FileOpenDialog1: TFileOpenDialog;
+    Label1: TLabel;
+    MainMenu1: TMainMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    Panel1: TPanel;
+    ListBox3: TListBox;
+    Panel2: TPanel;
+    N5: TMenuItem;
+    N4: TMenuItem;
+    N6: TMenuItem;
+    procedure N1Click(Sender: TObject);
+    procedure N2Click(Sender: TObject);
+    procedure N4Click(Sender: TObject);
+    procedure N5Click(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure N6Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure ListBox1KeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+    procedure N3Click(Sender: TObject);
+  private
+    { Private êÈåæ }
+  public
+    { Public êÈåæ }
+    mainDir: string;
+    function GetSameFile(const Name: string): string;
+    function IsArch(const Name: string): Boolean;
+    procedure disposeItems;
+    procedure Execute(const Name: string);
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses System.Generics.Collections, System.IOUtils;
+
+procedure TForm1.disposeItems;
+begin
+  for var i := 0 to ListBox3.Count - 1 do
+    Dispose(Pointer(ListBox3.Items.Objects[i]));
+  ListBox3.Items.Clear;
+end;
+
+procedure TForm1.Execute(const Name: string);
+const
+  arch = 'Win64\Release\';
+var
+  str, text, cur: string;
+  obj: ^TPair<string, string>;
+begin
+  if FileExists(Name) then
+  begin
+    mainDir := ExtractFileDir(Name);
+    str := GetSameFile(Name);
+  end
+  else
+  begin
+    mainDir := Name;
+    str := GetSameFile(mainDir);
+    ListBox2.Items.Add('åüçıÇµÇ‹Ç∑ ... ...');
+  end;
+  if str <> '' then
+  begin
+    ListBox2.Items.Add('ìoò^ÇµÇ‹Ç∑');
+    for var s in str.Split([',']) do
+    begin
+      if IsArch(s) then
+        continue;
+      cur := ExtractFileDir(s);
+      text := s + '\%s=>' + arch + s + '\%s';
+      cur := mainDir + '\' + s;
+      if not DirectoryExists(cur) then
+      begin
+        New(obj);
+        obj^.Key := mainDir;
+        obj^.Value := mainDir + arch + s;
+        ListBox2.Items.Add(mainDir + '\' + s);
+        ListBox3.Items.AddObject(Format(text, ['', '']), Pointer(obj));
+      end
+      else
+        for var t in GetSameFile(cur).Split([',']) do
+        begin
+          New(obj);
+          obj^.Key := cur;
+          obj^.Value := cur + '\' + t;
+          ListBox2.Items.Add(cur + '\' + t);
+          ListBox3.Items.AddObject(Format(text, [t, t]), Pointer(obj));
+        end;
+    end;
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  Name: string;
+begin
+  name := ChangeFileExt(Application.ExeName, '.txt');
+  if FileExists(name) then
+    ListBox1.Items.LoadFromFile(name);
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  if ListBox1.Count > 0 then
+    ListBox1.Items.SaveToFile(ChangeFileExt(Application.ExeName, '.txt'));
+  disposeItems;
+end;
+
+function TForm1.GetSameFile(const Name: string): string;
+var
+  rec: TSearchRec;
+  i, data: integer;
+  d: string;
+begin
+  result := '';
+  if Name = mainDir then
+  begin
+    data := faDirectory;
+    d := Name + '\*';
+  end
+  else
+  begin
+    data := faNormal;
+    if FileExists(Name) then
+      d := Name
+    else
+      d := Name + '\*.*';
+  end;
+  i := FindFirst(d, data, rec);
+  try
+    while i = 0 do
+    begin
+      if ((data = faDirectory) and ((rec.Name = '.') or (rec.Name = '..'))) or
+        ((data = faDirectory) and (rec.Attr <> faDirectory)) then
+      begin
+        i := FindNext(rec);
+        continue;
+      end;
+      if result = '' then
+        result := rec.Name
+      else
+        result := result + ',' + rec.Name;
+      i := FindNext(rec);
+    end;
+  finally
+    FindClose(rec);
+  end;
+end;
+
+function TForm1.IsArch(const Name: string): Boolean;
+const
+  fileArray: TArray<string> = ['Win32', 'Win64', 'Linux32', 'Linux64'];
+begin
+  for var s in fileArray do
+    if Name = s then
+      Exit(true);
+  result := false;
+end;
+
+procedure TForm1.ListBox1KeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if Key = VK_DELETE then
+    ListBox1.DeleteSelected;
+end;
+
+procedure TForm1.N1Click(Sender: TObject);
+begin
+  if FileOpenDialog1.Execute then
+  begin
+    case FileOpenDialog1.FileTypeIndex of
+      1:
+        ListBox1.Items.Add(ExtractFileDir(FileOpenDialog1.FileName));
+      2:
+        ListBox1.Items.Add(FileOpenDialog1.FileName);
+    end;
+    N4Click(Sender);
+  end;
+end;
+
+procedure TForm1.N2Click(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TForm1.N3Click(Sender: TObject);
+begin
+  if ListBox1.ItemIndex > -1 then
+  begin
+    disposeItems;
+    Execute(ListBox1.Items[ListBox1.ItemIndex]);
+    N5Click(Sender);
+  end;
+end;
+
+procedure TForm1.N4Click(Sender: TObject);
+begin
+  disposeItems;
+  ListBox2.Items.Clear;
+  for var s in ListBox1.Items do
+    Execute(s);
+end;
+
+procedure TForm1.N5Click(Sender: TObject);
+var
+  obj: ^TPair<string, string>;
+begin
+  for var i := 0 to ListBox3.Count - 1 do
+  begin
+    Pointer(obj) := ListBox3.Items.Objects[i];
+    CopyFile(PChar(obj^.Key), PChar(obj^.Value), false);
+  end;
+end;
+
+procedure TForm1.N6Click(Sender: TObject);
+begin
+  case ListBox2.Width of
+    150:
+      ListBox2.Width := 500;
+    500:
+      ListBox2.Width := 150;
+  end;
+end;
+
+end.
